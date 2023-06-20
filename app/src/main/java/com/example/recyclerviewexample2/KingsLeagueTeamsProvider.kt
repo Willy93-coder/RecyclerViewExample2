0a5820f67a74333238f6481a91924111daad8e04
Willy <guillecg13@gmail.com>
2023-06-20 16:57:30 +0200

    Created: kings league team provider

---
+++ app/src/main/java/com/example/recyclerviewexample2/KingsLeagueTeamsProvider.kt
@@ -0,0 +1,80 @@
+package com.example.recyclerviewexample2
+
+class KingsLeagueTeamsProvider {
+    companion object {
+        val KingsLeagueTeamsList = listOf(
+            KingsLeagueTeams(
+                "1K FC",
+                "Iker Casillas",
+                "8º",
+                "https://upload.wikimedia.org/wikipedia/commons/9/98/1K_F.C_logo.png"
+            ),
+            KingsLeagueTeams(
+                "Aniquiladores FC",
+                "Juan Guarnizo",
+                "2º",
+                "https://upload.wikimedia.org/wikipedia/commons/f/ff/Aniquiladores_fc.png"
+            ),
+            KingsLeagueTeams(
+                "El Barrio",
+                "Adri Contreras",
+                "4º",
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/El_Barrio_KL_Logo.svg/1001px-El_Barrio_KL_Logo.svg.png"
+            ),
+            KingsLeagueTeams(
+                "Jijantes",
+                "Gerard Romero",
+                "12º",
+                "https://upload.wikimedia.org/wikipedia/commons/4/47/JIJANTES_FC.png"
+            ),
+            KingsLeagueTeams(
+                "Kunisports",
+                "Kun Agüero",
+                "9º",
+                "https://upload.wikimedia.org/wikipedia/commons/2/29/Kunisports.png"
+            ),
+            KingsLeagueTeams(
+                "Los Troncos FC",
+                "Perxitaa",
+                "11º",
+                "https://upload.wikimedia.org/wikipedia/commons/8/84/Los_troncos_fckingsleague.png"
+            ),
+            KingsLeagueTeams(
+                "Pio FC",
+                "Rivers",
+                "5º",
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d6/PIO_FC.svg/640px-PIO_FC.svg.png"
+            ),
+            KingsLeagueTeams(
+                "Porcinos FC",
+                "Ibai Llanos",
+                "1º",
+                "https://seeklogo.com/images/P/porcinos-fc-logo-EA9B8086FD-seeklogo.com.png"
+            ),
+            KingsLeagueTeams(
+                "Rayo de Barcelona",
+                "Spursito",
+                "6º",
+                "https://upload.wikimedia.org/wikipedia/commons/1/15/Rayo_barcelona.png"
+            ),
+            KingsLeagueTeams(
+                "Saiyans FC",
+                "TheGrefg",
+                "10º",
+                "https://upload.wikimedia.org/wikipedia/commons/0/06/Saiyans_FC.png"
+            ),
+            KingsLeagueTeams(
+                "Ultimate Móstoles",
+                "DjMaRiiO",
+                "7º",
+                "https://upload.wikimedia.org/wikipedia/commons/1/14/Ultimatemostoles.png"
+            ),
+            KingsLeagueTeams(
+                "xBuyer Team",
+                "Hnos. Buyer",
+                "3º",
+                "https://upload.wikimedia.org/wikipedia/commons/f/fb/Xbuyer.png"
+            ),
+        )
+    }
+}
